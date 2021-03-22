c80709558df1f035d77ddf6db35671976593bc71
Grzegorz Kupczyk <kupczykgrzeg@gmail.com>
2021-03-23 00:24:29 +0100

    Animowany strumien

--- animacja/animacja.py
+++ animacja/animacja.py
@@ -8,21 +8,23 @@ from matplotlib.animation import FuncAnimation
 
 
 def Q(t):
-    if t < 2:
-        return 1
-    elif t < 5:
+    if t < 4:
+        return 0.2 * t
+    elif t < 8:
         return 0
-    elif t < 17:
-        return 0.5
-    return 5
+    elif t < 12:
+        return 4 - 0.3 * t
+    elif t < 16:
+        return 4 - 0.1 * t
+    return 0
 
 
 def zbiornik_model(x, t):
     A = 2  # pole powierzchni lustra wody
     g = 9.81  # warość przyśpieszenia ziemskiego
     s = 0.02  # pole powierzchni przekroju wypływu
-    Qin = Q(t)
-    h = x[0]
+    Qin = Q(t)  # dopływ wody
+    h = x[0]  # wysokość słupa wody
 
     dhdt = 1 / A * (Qin - s * sqrt(2 * g * h))
 
@@ -30,7 +32,7 @@ def zbiornik_model(x, t):
 
 
 class SuperContainer:
-    def __init__(self, h, ax):
+    def __init__(self, h, q_in, ax):
         container_points = [[-3, 2], [-3, -2], [3, -2], [3, 2]]
         pipe_in_points = [[-2.7, 2.3], [-5, 2.4], [-5, 2.2], [-2.7, 2.1]]
         pipe_out_points = [
@@ -43,6 +45,7 @@ class SuperContainer:
         ]
         fill_points = self.__get_fill_points()
         pointer_points = [[3, 2], [5, 2]]
+        stream_points = self.__stream(0)
 
         self.outline = plt.Polygon(
             container_points, closed=None, edgecolor="k", fill=False, lw=4
@@ -57,8 +60,13 @@ class SuperContainer:
         self.pipe_out = plt.Polygon(
             pipe_out_points, closed=None, edgecolor="k", facecolor="b", lw=2
         )
-        self.text = ax.text(4, 2, "h=foo")
+
+        self.stream = plt.Polygon(
+            stream_points, closed=None, edgecolor="b", fill=False, lw=5
+        )
+        self.text = ax.text(4, 2, "h=0")
         self.__h = h
+        self.__q = q_in
 
     def add_path(self, gca):
         gca.add_patch(self.fill)
@@ -66,6 +74,7 @@ class SuperContainer:
         gca.add_patch(self.pipe_in)
         gca.add_patch(self.pipe_out)
         gca.add_patch(self.pointer)
+        gca.add_patch(self.stream)
 
     def __wavy(self, x1, x2, y0, points, amp=1, offset=0, reverse=False):
         ax = np.linspace(x1, x2, points)
@@ -76,6 +85,16 @@ class SuperContainer:
             ax = np.flip(ax)
 
         return np.vstack((ax, ay)).T
+
+    def __stream(self, q_in):
+        q_max = 5
+        q = q_in / q_max
+        w_max = 1
+        points = 20
+        x = np.linspace(-2.7, -2.7 + q * w_max, points)
+        y = -4 * np.sin(np.linspace(0, np.pi / 2, points)) + 2.3
+
+        return np.vstack((x, y)).T
 
     def __get_fill_points(self, offset=0, h=0):
         h_max = 10
@@ -90,6 +109,7 @@ class SuperContainer:
 
     def animate(self, i):
         h = self.__h[i]
+        q = self.__q[i]
 
         h_max = 10
         y_min = -1.9
@@ -97,15 +117,16 @@ class SuperContainer:
 
         self.fill.set_xy(self.__get_fill_points(i, h))
         self.pointer.set_xy([[3, y], [5, y]])
+        self.stream.set_xy(self.__stream(q))
         self.text.set_position([3.5, y])
         self.text.set_text("h = %.1f" % h)
 
-        return (self.fill, self.pointer, self.text)
+        return (self.fill, self.pointer, self.stream, self.text)
 
 
 def init():
-    global superContainer, h, ax0
-    superContainer = SuperContainer(h, ax0)
+    global superContainer, h, Q, ax0
+    superContainer = SuperContainer(h, Q, ax0)
     superContainer.add_path(plt.gca())
     return superContainer.animate(0)
 
@@ -126,9 +147,12 @@ superContainer = None
 h = x[:, 0]
 Q = [Q(t) for t in t]
 ax1 = fig.add_subplot(212, autoscale_on=True)
-ax1.plot(t, h, label="h")
-ax1.plot(t, Q, label="Q")
+ax1.plot(t, h, label="h [m]")
+ax1.plot(t, Q, label="Q [m^3/s]")
+ax1.set_xlim([t[0], t[-1]])
+ax1.set_ylim((0, np.amax(h)))
 ax1.set_xlabel("t")
+ax1.legend(loc="upper left")
 
 ax0 = fig.add_subplot(211, autoscale_on=False, xlim=(-5, 5), ylim=(-5, 5))
 ax0.grid()
